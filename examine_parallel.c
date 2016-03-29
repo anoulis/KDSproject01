2bb20843082d6f0837f34ccad9e2d4c3f34b0540
anoulis <arisnoul@csd.auth.gr>
2016-03-29 22:49:35 +0300

    Fscanf replaced by fread and the processing time was reduced

--- examine_parallel.c
+++ examine_parallel.c
@@ -3,50 +3,58 @@
 #include <time.h>
 #include <omp.h>
 
-
-
 struct timespec start,end;
 
-
-void read_file(char* filename,float Array[])
+void read_file(char* filename)
 {
   FILE *fp;
   fp = fopen(filename, "r");
 
-  if (fp == NULL){
+  if (fp == NULL)
+  {
     puts("Error reading file");
     return;
   }
 
-  int count1=0;
-  int count2=0;
-  int i=0;
-  float x;
-  float percentage;
-clock_gettime(CLOCK_MONOTONIC, &start);
+   float *Array,x,percentage;
+   Array=(float*)calloc(15000000*3,sizeof(float));
+   long size =(15000000*30),size2;
+   char *buffer,c[9];
+   buffer=(char*)calloc(size,sizeof(char));
+   int flag=0,k=0,i=0,count1=0,count2=0;
 
-
-while (!feof(fp))
-  { fscanf(fp,"%f ",&x);
-    Array[count1]=x;
-    count1++;
-  }
-
-
-  #pragma omp parallel for firstprivate(count1) reduction(+:count2)
+ clock_gettime(CLOCK_MONOTONIC, &start);
+  fread(buffer, size, 1, fp);
+    for(i=0;i<size;i++)
+    { if(buffer[i]!=' ' && buffer[i]!='\n')
+      {
+        c[k]=buffer[i];
+        k++;
+      }
+      else
+      {
+        Array[flag]=atof(c);
+        flag++;
+        k=0;
+      }
+    }
+  count1=flag;
+ #pragma omp parallel for firstprivate(count1) reduction(+:count2)
   for (i=0;i<count1;i++)
   {
    if (Array[i]>=12 && Array[i]<=30)
     count2++;
   }
 
+  free(buffer);
+  free(Array);
 
   clock_gettime(CLOCK_MONOTONIC, &end);
   fclose(fp);
   printf("Total coordinates read: %d\n",count1);
   printf("Total coordinates inside interval: %d\n",count2);
   percentage = ((float)count2/(float)count1)*100;
-  printf("Percentage of coordinates inside interval: %f\n", percentage);
+  printf("Percentage of coordinates inside interval: %f%c\n", percentage,37);
 }
 
 void print_time()
@@ -64,9 +72,6 @@ void print_time()
 
 int main()
 {
-    float *Array;
-    Array=(float*)calloc(450000,sizeof(float));
-    read_file("data",Array);
+    read_file("data");
     print_time();
-    free(Array);
 }
