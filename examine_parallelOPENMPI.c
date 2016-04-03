1420d77a54c8185af0eb63b929f8e109f12de843
anoulis <arisnoul@csd.auth.gr>
2016-04-03 15:43:09 +0300

    Added defines,improved reading.First try to combine openmp and openmpi.Removed unused lines

--- examine_parallelOPENMPI.c
+++ examine_parallelOPENMPI.c
@@ -1,52 +1,89 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "mpi.h"
+#include <time.h>
+#include <mpi.h>
+#include <omp.h>
 
-#define FILESIZE 15000000*30
-#define BUFSIZE 15000000*3
-#define FILENAME "data"
-/*
-int main(int argc, char *argv[])
+
+#define limit1 12
+#define limit2 30
+#define number 15000000
+#define FILESIZE number*30
+
+struct timespec start,end;
+
+
+
+int read_file(char* filename,int argc, char* argv[])
 {
-MPI_File myfile;
-int rank, size;
-MPI_Init(&argc, &argv);
-MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-MPI_Comm_size(MPI_COMM_WORLD, &size);
-printf ("Hello world! I am %d of %d \n", rank, size);
-printf("paok");
-MPI_File_open (MPI_COMM_WORLD, FILENAME, MPI_MODE_CREATE | MPI_MODE_WRONLY,MPI_INFO_NULL, &myfile);
-
-
-MPI_File_close(&myfile);
-MPI_Finalize();
-return 0;
-
-//int MPI_File_open(MPI_Comm comm, const char *filename,int amode, MPI_Info info,MPI_File *fh)
-}*/
-
-
-
-
-int main(int argc, char* argv[])
-{
-  int rank, size, bufsize, nints,sz;
+  int rank, size, bufsize, nints,sz,count2=0,i,k=0,count1=0;
+  float *Array,percentage,x;
+  Array=(float*)calloc(number*3,sizeof(float));
   MPI_File myfile;
   MPI_Status status;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
+  bufsize=FILESIZE/size;
+  nints=bufsize/sizeof(char);
+  char *buffer,c[9];
+  buffer=(char*)calloc(nints,sizeof(char));
+  MPI_File_open (MPI_COMM_WORLD, filename, MPI_MODE_RDONLY,MPI_INFO_NULL, &myfile);
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  MPI_File_read_at(myfile, rank*bufsize,buffer, nints, MPI_CHAR, &status);
 
-  nints=BUFSIZE/sizeof(float);
-  float *buf;
-  buf=(float*)calloc(nints,sizeof(float));
-  MPI_File_open (MPI_COMM_WORLD, FILENAME, MPI_MODE_RDONLY,MPI_INFO_NULL, &myfile);
-  MPI_File_read_at(myfile, rank*BUFSIZE,buf, nints, MPI_FLOAT, &status);
+  for (i=0;i<nints;i++)
+  {
+    printf("%d->%c\n",i,buffer[i]);
+  }
 
+for(i=0;i<nints;i++)
+    { if(buffer[i]!=' ' && buffer[i]!='\n')
+      {
+        c[k]=buffer[i];
+        k++;
+      }
+      else
+      {
+       Array[count1]=atof(c);
+        count1++;
+        k=0;
+      }
+    }
+#pragma omp parallel for firstprivate(count1) reduction(+:count2)
+  for (i=0;i<count1;i++)
+  {
+   if (Array[i]>=limit1 && Array[i]<=limit2)
+    count2++;
+  }
 
+  clock_gettime(CLOCK_MONOTONIC, &end);
   MPI_File_close(&myfile);
   MPI_Finalize();
-  exit(0);
+
+  printf("Total coordinates read: %d\n",count1);
+  printf("Total coordinates inside interval: %d\n",count2);
+  percentage = ((float)count2/(float)count1)*100;
+  printf("Percentage of coordinates inside interval: %f%c\n", percentage,37);
+
 }
 
+void print_time()
+{
+  const int DAS_NANO_SECONDS_IN_SEC = 1000000000;
+  long timeElapsed_s = end.tv_sec -start.tv_sec;
+  long timeElapsed_n = end.tv_nsec -start.tv_nsec;
+  if ( timeElapsed_n < 0 )
+  {
+    timeElapsed_n = DAS_NANO_SECONDS_IN_SEC + timeElapsed_n;
+    timeElapsed_s--;
+  }
+  printf("Time: %ld.%09ld secs \n",timeElapsed_s,timeElapsed_n);
+}
 
+int main(int argc, char* argv[])
+{
+    read_file("data",argc,argv);
+   print_time();
+    exit(0);
+}
